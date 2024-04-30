dbac8359db560c9665001e7642622c9101d0fa84
hanjihun2000 <1155128719@link.cuhk.edu.hk>
2024-04-30 18:57:46 +0800

    fix errors in '/buy' section and db configuration; still have an error where UI is properly displayed in '/buy/:detail-item-id'

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 require("./db");
 const mongoose = require("mongoose");
-const Item_sale = mongoose.model("Item_buy");
+const Item_buy = mongoose.model("Item_buy");
 const Item_share = mongoose.model("Item_share");
 const User = mongoose.model("User");
 const session = require("express-session");
@@ -55,19 +55,25 @@ var upload = multer({ storage: storage });
 
 // default home page
 app.get("/", function (req, res) {
-  const loggedIn = !!req.session.username;
-  res.render("home", { loggedIn: loggedIn });
+  if (req.session.username === undefined) {
+    res.render("home", { loggedIn: false });
+  } else {
+    res.render("home", { loggedIn: true });
+  }
 });
 
 // item buying page
 app.get("/buy", function (req, res) {
   // check login state
   if (req.session.username === undefined) {
-    res.render("home", { error: "Please login or register first!" });
+    res.render("home", {
+      error: "Please login or register first!",
+      loggedIn: false,
+    });
   }
   // find all items ready for sale in database, tag finished will be omitted
   else {
-    Item_sale.find(
+    Item_buy.find(
       { status: { $ne: "finished" } },
       function (err, varToStoreResult) {
         if (err) {
@@ -78,11 +84,12 @@ app.get("/buy", function (req, res) {
           // start mapping images
           if (item.img.data !== undefined) {
             item.img.data = item.img.data.toString("base64"); // convert the data into base64
+            item._id = item._id.toString();
             item.img = item.img.toObject();
           }
           return item;
         });
-        res.render("buy", { shop: items });
+        res.render("buy", { shop: items, loggedIn: true });
       }
     );
   }
@@ -91,7 +98,7 @@ app.get("/buy", function (req, res) {
 // this is a test block for next function
 app.post("/buy", function (req, res) {
   key = Object.keys(req.body)[0];
-  Item_sale.findOneAndUpdate(
+  Item_buy.findOneAndUpdate(
     { _id: key },
     {
       $set: { status: "requested" },
@@ -105,10 +112,26 @@ app.post("/buy", function (req, res) {
   );
 });
 
+app.get("/buy/:id", (req, res) => {
+  let id = req.params.id;
+  const objectId = mongoose.Types.ObjectId(id);
+  Item_buy.find({ _id: objectId }, function (err, result) {
+    if (err) {
+      console.log(err);
+    }
+    let item = result[0];
+    if (item.img.data !== undefined) {
+      item.img.data = item.img.data.toString("base64"); // convert the data into base64
+      item.img = item.img.toObject();
+    }
+    res.render("detail", { shop: item, loggedIn: true });
+  });
+});
+
 // create form for request botton of a product, this is to easily digest which user requested from session inputs for reference
-app.post("/buy/:slug", function (req, res) {
+app.post("/buy/:id", function (req, res) {
   key = Object.keys(req.body)[0];
-  Item_sale.findOneAndUpdate(
+  Item_buy.findOneAndUpdate(
     { _id: key },
     {
       // update item information by adding new requests
@@ -127,48 +150,37 @@ app.post("/buy/:slug", function (req, res) {
   );
 });
 
-// show item detail page
-app.get("/buy/:slug", (req, res) => {
-  // get item name to search in database
-  let slug1 = req.path.split("/");
-  slug1 = slug1[slug1.length - 1];
-  Item_sale.find({ _id: slug1 }, function (err, result) {
-    if (err) {
-      console.log(err);
-    }
-    let item = result[0];
-    if (item.img.data !== undefined) {
-      item.img.data = item.img.data.toString("base64"); // convert the data into base64
-      item.img = item.img.toObject();
-    }
-    res.render("detail", { shop: item });
-  });
-});
-
 // sell page for users to post new items
 app.get("/sell", function (req, res) {
   if (req.session.username === undefined) {
-    res.render("home", { error: "Please login or register first!" });
+    res.render("home", {
+      error: "Please login or register first!",
+      loggedIn: false,
+    });
   } else {
-    res.render("sell");
+    res.render("sell", { loggedIn: true });
   }
 });
 
-// create form for user to upload new item for sale, including a document(picture) of the item.
 app.post("/sell", upload.single("image"), function (req, res, next) {
-  // imput validations
-  if (typeof req.body.title !== "string") {
-    res.render("sell", { error: "Name is not valid!" });
+  // input validations
+  if (
+    typeof req.body.title !== "string" ||
+    typeof req.body.description !== "string" ||
+    !req.session.username ||
+    typeof req.session.username.username !== "string"
+  ) {
+    res.render("sell", { error: "Invalid input!", loggedIn: true });
   } else if (
     typeof req.body.price !== "string" ||
     isNaN(req.body.price) ||
     parseInt(req.body.price) < 0
   ) {
-    res.render("sell", { error: "Price is ridiculous!" });
+    res.render("sell", { error: "Price is ridiculous!", loggedIn: true });
   }
   // add new item to database
   else {
-    new Item_sale({
+    new Item_buy({
       title: req.body.title,
       price: parseInt(req.body.price),
       description: req.body.description,
@@ -181,169 +193,174 @@ app.post("/sell", upload.single("image"), function (req, res, next) {
       updated_at: Date.now(),
     }).save(function (err, Item_sale, count) {
       if (err) {
-        console.log(err, count);
+        res.render("sell", { error: "Error saving item!", loggedIn: true });
+        return;
       }
       // update user information by adding new product in place
-      if (req.session.username !== undefined) {
-        console.log("yes");
-        User.findOneAndUpdate(
-          { username: req.session.username.username },
-          {
-            $push: {
-              items_buy: req.body.title,
-            },
+      User.findOneAndUpdate(
+        { username: req.session.username.username },
+        {
+          $push: {
+            items_sell: req.body.title,
           },
-          { new: true },
-          (err, doc) => {
-            if (err) {
-              console.log("Something wrong when updating data!");
-            }
+        },
+        { new: true },
+        (err, doc) => {
+          if (err) {
+            res.render("sell", {
+              error: "Error updating user information!",
+              loggedIn: true,
+            });
+            return;
           }
-        );
-      }
-      res.redirect("/");
-    });
-  }
-});
-
-// page for user to lend products, similar to sell page for current development purposes
-app.get("/lend", function (req, res) {
-  if (req.session.username === undefined) {
-    res.render("home", { error: "Please login or register first!" });
-  } else {
-    res.render("lend");
-  }
-});
-
-// form for user to post new lending products, similar to sell form
-app.post("/lend", upload.single("image"), function (req, res) {
-  if (typeof req.body.title !== "string") {
-    res.render("lend", { error: "Name is not valid!" });
-  } else if (
-    typeof req.body.price !== "string" ||
-    isNaN(req.body.price) ||
-    parseInt(req.body.price) < 0
-  ) {
-    res.render("lend", { error: "Price is ridiculous!" });
-  } else {
-    new Item_share({
-      title: req.body.title,
-      price: parseInt(req.body.price),
-      description: req.body.description,
-      owner: req.session.username.username,
-      img: {
-        data: fs.readFileSync("./uploads/" + req.file.filename),
-        contentType: req.file.mimetype,
-      },
-      status: "posted",
-      updated_at: Date.now(),
-    }).save(function (err, Item_sale, count) {
-      if (err) {
-        console.log(err, count);
-      }
-      if (req.session.username !== undefined) {
-        console.log("yes");
-        User.findOneAndUpdate(
-          { username: req.session.username.username },
-          {
-            $push: {
-              items_share: req.body.title,
-            },
-          },
-          { new: true },
-          (err, doc) => {
-            if (err) {
-              console.log("Something wrong when updating data!");
-            }
-          }
-        );
-      }
-      res.redirect("/");
-    });
-  }
-});
-
-// page for item sharing, similar to buying page for current development purposes
-app.get("/share", function (req, res) {
-  if (req.session.username === undefined) {
-    res.render("home", { error: "Please login or register first!" });
-  } else {
-    Item_share.find(
-      { status: { $ne: "finished" } },
-      function (err, varToStoreResult) {
-        if (err) {
-          console.log(err);
+          res.redirect("/");
         }
-        let items = varToStoreResult;
-        items = items.map((item) => {
-          // start mapping images
-          if (item.img.data !== undefined) {
-            item.img.data = item.img.data.toString("base64"); // convert the data into base64
-            item.img = item.img.toObject();
-          }
-          return item;
-        });
-        res.render("share", { shop: items });
-      }
-    );
+      );
+    });
   }
 });
 
-// create form for request sharing item button, similar to the form when buying item
-app.post("/share/:slug", function (req, res) {
-  key = Object.keys(req.body)[0];
-  Item_share.findOneAndUpdate(
-    { _id: key },
-    {
-      $set: { status: "requested" },
-      $push: { requesters: req.session.username.email },
-    },
-    { upsert: true },
-    (err, doc) => {
-      if (err) {
-        console.log("Something wrong when updating data!");
-      } else {
-        // res.redirect("/buy");
-        res.redirect("/personal");
-      }
-    }
-  );
-});
+// // page for user to lend products, similar to sell page for current development purposes
+// app.get("/lend", function (req, res) {
+//   if (req.session.username === undefined) {
+//     res.render("home", { error: "Please login or register first!" });
+//   } else {
+//     res.render("lend");
+//   }
+// });
 
-// sharing item detail page, similar to buying page
-app.get("/share/:slug", (req, res) => {
-  let slug1 = req.path.split("/");
-  slug1 = slug1[slug1.length - 1];
-  Item_share.find({ _id: slug1 }, function (err, result) {
-    if (err) {
-      console.log(err);
-    }
-    let item = result[0];
-    if (item.img.data !== undefined) {
-      item.img.data = item.img.data.toString("base64"); // convert the data into base64
-      item.img = item.img.toObject();
-    }
-    res.render("detail", { shop: item });
-  });
-});
+// // form for user to post new lending products, similar to sell form
+// app.post("/lend", upload.single("image"), function (req, res) {
+//   if (typeof req.body.title !== "string") {
+//     res.render("lend", { error: "Name is not valid!" });
+//   } else if (
+//     typeof req.body.price !== "string" ||
+//     isNaN(req.body.price) ||
+//     parseInt(req.body.price) < 0
+//   ) {
+//     res.render("lend", { error: "Price is ridiculous!" });
+//   } else {
+//     new Item_share({
+//       title: req.body.title,
+//       price: parseInt(req.body.price),
+//       description: req.body.description,
+//       owner: req.session.username.username,
+//       img: {
+//         data: fs.readFileSync("./uploads/" + req.file.filename),
+//         contentType: req.file.mimetype,
+//       },
+//       status: "posted",
+//       updated_at: Date.now(),
+//     }).save(function (err, Item_sale, count) {
+//       if (err) {
+//         console.log(err, count);
+//       }
+//       if (req.session.username !== undefined) {
+//         console.log("yes");
+//         User.findOneAndUpdate(
+//           { username: req.session.username.username },
+//           {
+//             $push: {
+//               items_share: req.body.title,
+//             },
+//           },
+//           { new: true },
+//           (err, doc) => {
+//             if (err) {
+//               console.log("Something wrong when updating data!");
+//             }
+//           }
+//         );
+//       }
+//       res.redirect("/");
+//     });
+//   }
+// });
+
+// // page for item sharing, similar to buying page for current development purposes
+// app.get("/share", function (req, res) {
+//   if (req.session.username === undefined) {
+//     res.render("home", { error: "Please login or register first!" });
+//   } else {
+//     Item_share.find(
+//       { status: { $ne: "finished" } },
+//       function (err, varToStoreResult) {
+//         if (err) {
+//           console.log(err);
+//         }
+//         let items = varToStoreResult;
+//         items = items.map((item) => {
+//           // start mapping images
+//           if (item.img.data !== undefined) {
+//             item.img.data = item.img.data.toString("base64"); // convert the data into base64
+//             item.img = item.img.toObject();
+//           }
+//           return item;
+//         });
+//         res.render("share", { shop: items });
+//       }
+//     );
+//   }
+// });
+
+// // create form for request sharing item button, similar to the form when buying item
+// app.post("/share/:slug", function (req, res) {
+//   key = Object.keys(req.body)[0];
+//   Item_share.findOneAndUpdate(
+//     { _id: key },
+//     {
+//       $set: { status: "requested" },
+//       $push: { requesters: req.session.username.email },
+//     },
+//     { upsert: true },
+//     (err, doc) => {
+//       if (err) {
+//         console.log("Something wrong when updating data!");
+//       } else {
+//         // res.redirect("/buy");
+//         res.redirect("/personal");
+//       }
+//     }
+//   );
+// });
+
+// // sharing item detail page, similar to buying page
+// app.get("/share/:slug", (req, res) => {
+//   let slug1 = req.path.split("/");
+//   slug1 = slug1[slug1.length - 1];
+//   Item_share.find({ _id: slug1 }, function (err, result) {
+//     if (err) {
+//       console.log(err);
+//     }
+//     let item = result[0];
+//     if (item.img.data !== undefined) {
+//       item.img.data = item.img.data.toString("base64"); // convert the data into base64
+//       item.img = item.img.toObject();
+//     }
+//     res.render("detail", { shop: item });
+//   });
+// });
 
 // personal history page
 app.get("/personal", function (req, res) {
   if (req.session.username === undefined) {
-    res.render("home", { error: "Please login or register first!" });
+    res.render("home", {
+      error: "Please login or register first!",
+      loggedIn: false,
+    });
   }
   // find all items user requested, and all other users that requested certain items of this user. will showcase two sections seperately.
   // buyers are able to see updates of requested items, but cannot contact sellers directly.
   // sellers are able to see requesters' emails and initiate contacts. They can also update items' availability by clicking denied (remove requesters) or finished (remove item since transaction is completed).
   else {
-    Item_sale.find(
+    Item_buy.find(
       { owner: req.session.username.username, status: "requested" },
       function (err, varToStoreResult) {
         if (err) {
           console.log(err);
         }
         const items = varToStoreResult;
-        Item_sale.find(
+        Item_buy.find(
           { requesters: req.session.username.email, status: "requested" },
           function (err, varToStoreResult) {
             if (err) {
@@ -369,6 +386,7 @@ app.get("/personal", function (req, res) {
                       request: request,
                       shop2: items2,
                       request2: request2,
+                      loggedIn: true,
                     });
                   }
                 );
@@ -387,7 +405,7 @@ app.post("/personal", function (req, res) {
   if (Object.values(req.body)[0] === "Finished") {
     result = "finished";
     key = Object.keys(req.body)[0];
-    Item_sale.findOneAndUpdate(
+    Item_buy.findOneAndUpdate(
       { _id: key },
       {
         $set: { status: result, requesters: [] },
@@ -398,24 +416,25 @@ app.post("/personal", function (req, res) {
           console.log("Something wrong when updating data!");
         } else {
           console.log(doc);
-          if (doc !== null && doc.owner.length === 0) {
-            res.redirect("/personal");
-          } else {
-            Item_share.findOneAndUpdate(
-              { _id: key },
-              {
-                $set: { status: result, requesters: [] },
-              },
-              { upsert: true },
-              (err, doc) => {
-                if (err) {
-                  console.log("Something wrong when updating data!");
-                } else {
-                  res.redirect("/personal");
-                }
-              }
-            );
-          }
+          res.redirect("/personal");
+          // if (doc !== null && doc.owner.length === 0) {
+          //   res.redirect("/personal");
+          // } else {
+          //   Item_share.findOneAndUpdate(
+          //     { _id: key },
+          //     {
+          //       $set: { status: result, requesters: [] },
+          //     },
+          //     { upsert: true },
+          //     (err, doc) => {
+          //       if (err) {
+          //         console.log("Something wrong when updating data!");
+          //       } else {
+          //         res.redirect("/personal");
+          //       }
+          //     }
+          //   );
+          // }
         }
       }
     );
@@ -423,7 +442,7 @@ app.post("/personal", function (req, res) {
     // remove all requesters from list, transaction is denied and item is reposted for requests
     result = "posted";
     key = Object.keys(req.body)[0];
-    Item_sale.findOneAndUpdate(
+    Item_buy.findOneAndUpdate(
       { _id: key },
       {
         $set: { status: result, requesters: [] },
@@ -433,24 +452,25 @@ app.post("/personal", function (req, res) {
         if (err) {
           console.log("Something wrong when updating data!");
         } else {
-          if (doc !== null && doc.owner.length === 0) {
-            res.redirect("/personal");
-          } else {
-            Item_share.findOneAndUpdate(
-              { _id: key },
-              {
-                $set: { status: result, requesters: [] },
-              },
-              { upsert: true },
-              (err, doc) => {
-                if (err) {
-                  console.log("Something wrong when updating data!");
-                } else {
-                  res.redirect("/personal");
-                }
-              }
-            );
-          }
+          res.redirect("/personal");
+          // if (doc !== null && doc.owner.length === 0) {
+          //   res.redirect("/personal");
+          // } else {
+          //   Item_share.findOneAndUpdate(
+          //     { _id: key },
+          //     {
+          //       $set: { status: result, requesters: [] },
+          //     },
+          //     { upsert: true },
+          //     (err, doc) => {
+          //       if (err) {
+          //         console.log("Something wrong when updating data!");
+          //       } else {
+          //         res.redirect("/personal");
+          //       }
+          //     }
+          //   );
+          // }
         }
       }
     );
