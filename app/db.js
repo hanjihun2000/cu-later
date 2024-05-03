f1d7d8544afb7b8bd93c49fcd835c8097bb6ae69
Anson Cheng <Anson.12666@icloud.com>
2024-05-03 18:43:24 +0800

    chore: remove unused code

--- app/db.js
+++ app/db.js
@@ -46,28 +46,4 @@ mongoose.model("Item_buy", Item_buy);
 mongoose.model("Item_share", Item_share);
 mongoose.model("User", User);
 
-// // is the environment variable, NODE_ENV, set to PRODUCTION?
-// let dbconf;
-// if (process.env.NODE_ENV === 'PRODUCTION') {
-//  // if we're in PRODUCTION mode, then read the configration from a file
-//  // use blocking file io to do this...
-//  const fs = require('fs');
-//  const path = require('path');
-//  const fn = path.join(__dirname, 'config.json');
-//  const data = fs.readFileSync(fn);
-//  // our configuration file will be in json, so parse it and set the
-//  // conenction string appropriately!
-//  const conf = JSON.parse(data);
-//  console.log(conf.dbconf);
-//  dbconf = conf.dbconf;
-// } else {
-//  // if we're not in PRODUCTION mode, then use
-//  dbconf = 'mongodb://localhost/final';
-// }
-
-// Using local database
-// mongoose.connect('mongodb://localhost/final')
-// Using cloud database
-// not showing in opensource projects
-
 mongoose.connect(process.env.DB);
